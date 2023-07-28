e0517829d0221449635fd0432eeea94064e9fe95
Mihail <mihail.strijanov@gmail.com>
2023-07-28 15:54:16 +0300

    Created models

---
+++ games/buster/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+from datetime import date
+
+class Category(models.Model):
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Developers(models.Model):
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="Developers/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Разработчик"
+        verbose_name_plural = "Разработчик"
+
+class Genre(models.Model):
+    name = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+class Games(models.Model):
+    title = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    platforms = models.CharField("Название", max_length=20, default="Windows")
+    engine = models.CharField("Название", max_length=100)
+    preview = models.ImageField("", upload_to="buster/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2012)
+    genres = models.ManyToManyField(Genre, verbose_name="жанры")
+    budget = models.PositiveIntegerField("Бюджет", default=0, help_text="указывать сумму в $")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=130, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
+
+class GamesShorts(models.Model):
+    title = models.CharField("Заголовок", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="games_shorts/")
+    games = models.ForeignKey(Games, verbose_name="Игра", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Кадр из игры"
+        verbose_name_plural = "Кадры из игры"
+
+class RatingStar(models.Model):
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезды рейтинга"
+
+class Rating(models.Model):
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    games = models.ForeignKey(Games, on_delete=models.CharField, verbose_name="игра")
+
+    def __str__(self):
+        return f"{self.star} - {self.games}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинги"
+
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True)
+    games = models.ForeignKey(Games, verbose_name="игра", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.games}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
